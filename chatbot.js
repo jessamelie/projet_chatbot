895aa20a9170000f0af390686a954675576d8fbd
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-04-06 12:41:18 +0200

    En cours de problème

--- chatbot.js
+++ chatbot.js
@@ -1,25 +1,41 @@
 const APIkey = "CCxBDV1v5cemAWgT"
 const userID = "100012"
 let urlAPI = `http://api.brainshop.ai/get?bid=174304&key=CCxBDV1v5cemAWgT&uid=100012&msg=hello`
+const conversation = document.getElementById('conversation')
 
 //MESSAGE D'ACCUEIL BOT
 const firstBotMsg = document.getElementById("firstBotMsg")
  firstBotMsg.innerText ="Hi Buddy, how can I help you?"
 
- //AFFICHAGE MESSAGE BOT
- const botMsg = () => {
-    
- }
+//API
+const sendAPI = async () => {
+let response = await fetch(urlAPI)
+let data = await response.json()
+//botMsg(data.cnt)
+return data
 
+}
 
- //API
- const sendAPI = async () => {
-    let response = await fetch(urlAPI)
-    let data = await response.json()
-responseBot(data)
- }
+sendAPI()
 
+//ENVOI MESSAGE UTILISATEUR
+const messageInput = () => {
+const messageUser = document.getElementById('messageInput')
+const message = messageUser.value 
+console.log(message)
+conversation.innerHTML +=
+    `<div class="messageUser">
+        <div class="avatar">
+        <img class="img-circle avatar" alt="chat avatar" src="directeur.png">
+        </div>
+        <div class="message-text-wrapper">
+            <div class="message-text">
+                ${message}
+            </div>
+        </div>
+        <p class="time-stamp"><i class="fa fa-check"></i> 2 minute ago</p>
+      </div>`
+    messageUser.value = ""
+}
 
- const responseBot = (data) => {
-
- }
+document.getElementById('send').addEventListener("click", messageInput )
