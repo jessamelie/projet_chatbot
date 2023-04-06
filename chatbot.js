079e58b49177e252d3cfb5a4cdc1ca4f37a6f4df
jessamelie <jessamelie.sola@gmail.com>
2023-04-06 11:28:20 +0200

    update : AFI fonctionnelle

--- chatbot.js
+++ chatbot.js
@@ -1,3 +1,25 @@
-//MESSAGE DE LANCEMENT BOT
+const APIkey = "CCxBDV1v5cemAWgT"
+const userID = "100012"
+let urlAPI = `http://api.brainshop.ai/get?bid=174304&key=CCxBDV1v5cemAWgT&uid=100012&msg=hello`
+
+//MESSAGE D'ACCUEIL BOT
 const firstBotMsg = document.getElementById("firstBotMsg")
  firstBotMsg.innerText ="Hi Buddy, how can I help you?"
+
+ //AFFICHAGE MESSAGE BOT
+ const botMsg = () => {
+    
+ }
+
+
+ //API
+ const sendAPI = async () => {
+    let response = await fetch(urlAPI)
+    let data = await response.json()
+responseBot(data)
+ }
+
+
+ const responseBot = (data) => {
+
+ }
