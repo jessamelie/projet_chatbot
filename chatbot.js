7fa7eceaa414ffb9cd7692ae9399af2a0b120a4e
jessamelie <jessamelie.sola@gmail.com>
2023-04-06 14:42:31 +0200

    update: JS fonctionnel

--- chatbot.js
+++ chatbot.js
@@ -1,29 +1,21 @@
+let message = ""
+//VARIABLES ACCESS API
 const APIkey = "CCxBDV1v5cemAWgT"
 const userID = "100012"
-let urlAPI = `http://api.brainshop.ai/get?bid=174304&key=CCxBDV1v5cemAWgT&uid=100012&msg=hello`
-const conversation = document.getElementById('conversation')
 
-//MESSAGE D'ACCUEIL BOT
+
+//BOT : MESSAGE D'ACCUEIL FIXE
 const firstBotMsg = document.getElementById("firstBotMsg")
  firstBotMsg.innerText ="Hi Buddy, how can I help you?"
 
-//API
-const sendAPI = async () => {
-let response = await fetch(urlAPI)
-let data = await response.json()
-//botMsg(data.cnt)
-return data
+//USER : MESSAGE REQUETE
 
-}
+const sendAPI = async() => {
+    const messageUser = document.getElementById('messageInput')
+    message = messageUser.value 
+    let urlAPI = `http://api.brainshop.ai/get?bid=174304&key=${APIkey}&uid=${userID}&msg=${message}`
 
-sendAPI()
-
-//ENVOI MESSAGE UTILISATEUR
-const messageInput = () => {
-const messageUser = document.getElementById('messageInput')
-const message = messageUser.value 
-console.log(message)
-conversation.innerHTML +=
+    conversation.innerHTML +=
     `<div class="messageUser">
         <div class="avatar">
         <img class="img-circle avatar" alt="chat avatar" src="directeur.png">
@@ -36,6 +28,55 @@ conversation.innerHTML +=
         <p class="time-stamp"><i class="fa fa-check"></i> 2 minute ago</p>
       </div>`
     messageUser.value = ""
-}
 
-document.getElementById('send').addEventListener("click", messageInput )
+     let response = await fetch(urlAPI)
+     let data = await response.json()
+
+     conversation.innerHTML +=
+     `<div class="messageUser">
+         <div class="avatar">
+         <img class="img-circle avatar" alt="chat avatar" src="directeur.png">
+         </div>
+         <div class="message-text-wrapper">
+             <div class="message-text">
+                 ${data.cnt}
+             </div>
+         </div>
+         <p class="time-stamp"><i class="fa fa-check"></i> 2 minute ago</p>
+       </div>`
+
+
+}
+document.getElementById('send').addEventListener("click", sendAPI )
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const conversation = document.getElementById('conversation')
+
+
+
+// //API
+
+// //botMsg(data.cnt)
+// return data
+
+// }
+
+// sendAPI()
+
+// //ENVOI MESSAGE UTILISATEUR
+// const messageInput = () => {
+
+// console.log(message)
+
+// }
